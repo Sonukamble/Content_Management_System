6bfcf5ce7d9be52e9da28bb3ac20dc2717424d61
Sonukamble <ddkamble2141@gmail.com>
2025-01-22 14:03:25 +0530

    added the user-admin

--- content/urls.py
+++ content/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('api/content/', views.create_content, name='create_content'),
-    path('api/getcontent/', views.get_content, name='get_content'),
+    path('api/content/', views.get_content, name='get_content'),
     path('api/content/search/', views.search_content, name='search_content'),
     path('api/content/<int:pk>/', views.edit_content, name='edit_content'),
     path('api/content/<int:pk>/', views.delete_content, name='delete_content'),
